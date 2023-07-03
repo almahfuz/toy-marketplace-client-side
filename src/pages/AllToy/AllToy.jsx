e5748450a5f315bac8c6bcfc5259618375346bf9
almahfuz <abujafaralmahfuz@gmail.com>
2023-07-03 16:30:34 +0100

    all data load in alltoy pages

--- src/pages/AllToy/AllToy.jsx
+++ src/pages/AllToy/AllToy.jsx
@@ -1,178 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import AllToyData from "./AllToyData";
 
 const AllToy = () => {
+  const [allToyData, setAllToyData] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/alltoy")
+      .then((res) => res.json())
+      .then((data) => setAllToyData(data));
+  }, []);
+
   return (
-    <div  className='min-h-[calc(100vh-136px)]'>
+    <div className="min-h-[calc(100vh-136px)]">
       <div className="mt-5 md:mt-10">
         <div class=" capitalize text-2xl pt-10 pb-7 text-center font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
           All Toys
         </div>
         <hr class="w-1/4 h-1 mx-auto bg-blueGray-200 border-0 rounded dark:bg-gray-700" />
-      <div className="flex items-center justify-center pt-5 md:pt-10">
-      <div className="relative">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="absolute top-0 bottom-0 w-6 h-6 my-auto text-gray-400 left-3"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                >
-                    <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                    />
-                </svg>
-                <input
-                    type="text"
-                    placeholder="Search"
-                    className="w-full py-3 pl-12 pr-4 text-gray-500 border rounded-md outline-none bg-gray-50 focus:bg-white focus:border-neutral-700"
-                />
-            </div>
-      </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
-        <div className="cursor-pointer rounded-xl bg-white p-3 shadow-lg hover:shadow-xl">
-          <div className="relative flex items-end overflow-hidden rounded-xl">
-            <img
-              src="https://thumbnails.production.thenounproject.com/gA9eZOvsBYSHrMumgrslmRGoBto=/fit-in/1000x1000/photos.production.thenounproject.com/photos/BCBA88B6-5B41-4B50-A786-E60CAA0ECDA3.jpg"
-              alt="wallpaper"
+        <div className="flex items-center justify-center pt-5 md:pt-10">
+          <div className="relative">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="absolute top-0 bottom-0 w-6 h-6 my-auto text-gray-400 left-3"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+            <input
+              type="text"
+              placeholder="Search"
+              className="w-full py-3 pl-12 pr-4 text-gray-500 border rounded-md outline-none bg-gray-50 focus:bg-white focus:border-neutral-700"
             />
-
-            <div className="absolute bottom-3 left-3 inline-flex items-center rounded-lg bg-white p-2 shadow-md">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5 text-yellow-400"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-              </svg>
-
-              <span className="ml-1 text-sm text-slate-400">4.9</span>
-            </div>
-          </div>
-
-          <div className="mt-1 p-2">
-            <h2 className="text-slate-700">The Malta Hotel</h2>
-            <p className="mt-1 text-sm text-slate-400">Italy, Europe</p>
-
-            <div className="mt-3 flex items-end justify-between">
-              <p>
-                <span className="text-lg font-bold text-orange-500">
-                  $1,421
-                </span>
-                <span className="text-sm text-slate-400">/night</span>
-              </p>
-
-              <div className="group inline-flex rounded-xl bg-orange-100 p-2 hover:bg-orange-200">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-4 w-4 text-orange-400 group-hover:text-orange-500"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path d="M5 4a2 2 0 012-2h6a2 2 0 012 2v14l-5-2.5L5 18V4z" />
-                </svg>
-              </div>
-            </div>
           </div>
         </div>
-
-        <div className="cursor-pointer rounded-xl bg-white p-3 shadow-lg hover:shadow-xl">
-          <div className="relative flex items-end overflow-hidden rounded-xl">
-            <img
-              src="https://thumbnails.production.thenounproject.com/gA9eZOvsBYSHrMumgrslmRGoBto=/fit-in/1000x1000/photos.production.thenounproject.com/photos/BCBA88B6-5B41-4B50-A786-E60CAA0ECDA3.jpg"
-              alt="wallpaper"
-            />
-
-            <div className="absolute bottom-3 left-3 inline-flex items-center rounded-lg bg-white p-2 shadow-md">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5 text-yellow-400"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-              </svg>
-
-              <span className="ml-1 text-sm text-slate-400">4.9</span>
-            </div>
-          </div>
-
-          <div className="mt-1 p-2">
-            <h2 className="text-slate-700">The Malta Hotel</h2>
-            <p className="mt-1 text-sm text-slate-400">Italy, Europe</p>
-
-            <div className="mt-3 flex items-end justify-between">
-              <p>
-                <span className="text-lg font-bold text-orange-500">
-                  $1,421
-                </span>
-                <span className="text-sm text-slate-400">/night</span>
-              </p>
-
-              <div className="group inline-flex rounded-xl bg-orange-100 p-2 hover:bg-orange-200">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-4 w-4 text-orange-400 group-hover:text-orange-500"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path d="M5 4a2 2 0 012-2h6a2 2 0 012 2v14l-5-2.5L5 18V4z" />
-                </svg>
-              </div>
-            </div>
+        <div className="mt-5 md:mt-10">
+          <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-6 p-6">
+            {allToyData.map((allToyData) => (
+              <AllToyData
+                key={allToyData._id}
+                allToyData={allToyData}
+              ></AllToyData>
+            ))}
           </div>
         </div>
-
-        <div className="cursor-pointer rounded-xl bg-white p-3 shadow-lg hover:shadow-xl">
-          <div className="relative flex items-end overflow-hidden rounded-xl">
-            <img
-              src="https://thumbnails.production.thenounproject.com/gA9eZOvsBYSHrMumgrslmRGoBto=/fit-in/1000x1000/photos.production.thenounproject.com/photos/BCBA88B6-5B41-4B50-A786-E60CAA0ECDA3.jpg"
-              alt="wallpaper"
-            />
-
-            <div className="absolute bottom-3 left-3 inline-flex items-center rounded-lg bg-white p-2 shadow-md">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5 text-yellow-400"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-              </svg>
-
-              <span className="ml-1 text-sm text-slate-400">4.9</span>
-            </div>
-          </div>
-
-          <div className="mt-1 p-2">
-            <h2 className="text-slate-700">The Malta Hotel</h2>
-            <p className="mt-1 text-sm text-slate-400">Italy, Europe</p>
-
-            <div className="mt-3 flex items-end justify-between">
-              <p>
-                <span className="text-lg font-bold text-orange-500">
-                  $1,421
-                </span>
-                <span className="text-sm text-slate-400">/night</span>
-              </p>
-
-              <div className="group inline-flex rounded-xl bg-orange-100 p-2 hover:bg-orange-200">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-4 w-4 text-orange-400 group-hover:text-orange-500"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path d="M5 4a2 2 0 012-2h6a2 2 0 012 2v14l-5-2.5L5 18V4z" />
-                </svg>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
       </div>
     </div>
   );
